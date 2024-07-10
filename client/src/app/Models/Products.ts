c3e67526afad63de41400c153e3d07358841f695
Narender Reddy <narendera.reddy18@gmail.com>
2024-07-09 19:25:42 -0500

    Client Basics

---
+++ client/src/app/Models/Products.ts
@@ -0,0 +1,9 @@
+export interface Products {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    pictureUrl: string;
+    productType: string;
+    productBrand: string;
+  }
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Products } from './Models/Products';
+import { Pagination } from './Models/Pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Skinet';
+  products: Products[] = [];
+
+  constructor(private http: HttpClient){ }
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Products[]>>('https://localhost:7246/api/Products?pageSize=50').subscribe({
+      next: response => this.products =response.data,
+      error: erro => console.log(erro),
+      complete: () => {
+        console.log('complete')
+      }
+    });
+  }
 }
